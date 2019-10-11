8436e0e495135fe8ec924c7c13f25812503f7e1f
Logan McDonald <loganmcdona11@gmail.com>
2019-10-11 17:43:49 -0400

    adjust sql script and add the db

--- json-to-sql.py
+++ json-to-sql.py
@@ -1,7 +1,7 @@
 import sqlite3
 import json
 
-conn = sqlite3.connect('test.db')
+conn = sqlite3.connect('child_victims_act_data.db')
 c = conn.cursor()
 c.execute('''CREATE TABLE cases(title text, court text, case_id text, case_type text, case_status text, filing_status text, judge text)''')
 c.execute('''CREATE TABLE arbiters(case_id text, arbiter_type text,  names text, consented_by text)''')
